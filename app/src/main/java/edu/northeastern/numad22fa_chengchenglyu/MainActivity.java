689441502effe7791a1503ca0404be27ebfbf729
ChengchengLyu <chengpsu@163.com>
2022-09-16 22:33:45 -0700

    button and toast

--- app/src/main/java/edu/northeastern/numad22fa_chengchenglyu/MainActivity.java
+++ app/src/main/java/edu/northeastern/numad22fa_chengchenglyu/MainActivity.java
@@ -3,6 +3,9 @@ package edu.northeastern.numad22fa_chengchenglyu;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button aboutMe = findViewById(R.id.aboutMeButton);
+        aboutMe.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(), "Chengcheng Lyu: lu.chengc@northeastern.edu",
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
