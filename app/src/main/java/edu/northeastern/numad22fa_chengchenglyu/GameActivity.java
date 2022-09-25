f29e6cf99808730e78240c93a005c18fe3c31fe3
ChengchengLyu <chengpsu@163.com>
2022-09-24 23:35:54 -0700

    icon

--- app/src/main/java/edu/northeastern/numad22fa_chengchenglyu/GameActivity.java
+++ app/src/main/java/edu/northeastern/numad22fa_chengchenglyu/GameActivity.java
@@ -2,13 +2,21 @@ package edu.northeastern.numad22fa_chengchenglyu;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
 
 public class GameActivity extends AppCompatActivity {
+
+    ListView lv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
+        Button buy1 = findViewById(R.id.buy1);
+        @SuppressLint("WrongViewCast") EditText score = findViewById(R.id.scoreText);
     }
 }
--- app/src/main/java/edu/northeastern/numad22fa_chengchenglyu/MainActivity.java
+++ app/src/main/java/edu/northeastern/numad22fa_chengchenglyu/MainActivity.java
@@ -2,12 +2,14 @@ package edu.northeastern.numad22fa_chengchenglyu;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +22,16 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Toast.makeText(getApplicationContext(), "Chengcheng Lyu: lu.chengc@northeastern.edu",
                         Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        Button adcap = findViewById(R.id.AdCapButton);
+        adcap.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,GameActivity.class);
+                startActivity(intent);
+                finish();
             }
         });
     }
